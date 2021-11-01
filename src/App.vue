bc674ff740e2e651bb5a481d5ee9b1691f22e6de
Karthikeyan Kothandaraman <karthikeyan@yuvabe.com>
2021-11-01 22:01:58 +0530

    11/1/2021

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="bgimg">
+    <Header/> 
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Header
   }
 }
 </script>
@@ -23,4 +24,13 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
+body {
+  background: url('https://c1.wallpaperflare.com/preview/467/176/26/appetizer-dark-delicious-dining.jpg') no-repeat center center fixed;
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover;
+}
+
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,17 @@
+<template>
+    <h1>Get Delighted</h1>
+</template>
+
+<script>
+export default {
+    name: 'Header'
+}
+</script>
+
+<style scoped>
+h1 {
+    color: #332020;
+    font-family: cursive;
+    font-size: 500%;
+}
+</style>
